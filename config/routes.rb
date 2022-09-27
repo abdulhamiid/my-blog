77ef305a81e1725b3075e45fd41ffae0d42ef112
abdulhamiid <adioabdulhamid@yahoo.com>
2022-09-27 12:56:14 +0100

    Add controller to display user's comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   root "articles#index"
-  
-  resources :articles
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :articles do
+    resources :comments
+  end
 end
